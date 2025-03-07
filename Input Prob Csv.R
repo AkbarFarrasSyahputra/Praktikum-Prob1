c4ee672ce174f663d375493f4038bcbec0bc3bcd
AkbarFarrasSyahputra <farrassyahputra230426@gmail.com>
2025-03-07 23:24:04 +0700

    Add files via upload

---
+++ Input Prob Csv.R
@@ -0,0 +1,3 @@
+data_akbarrr = read.csv("C:/Users/farra/OneDrive/Dokumen/akbarrr.csv", sep=';')
+View(data_akbarrr)
+
---
+++ Input Prob xlsv.R
@@ -0,0 +1,4 @@
+data_akbar=read.delim("clipboard")
+View(data_akbar)
+mean(data_akbar$Tinggi.Badan)
+str(data_akbar)
